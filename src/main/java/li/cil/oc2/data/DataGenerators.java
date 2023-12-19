d48f61707dcf6914d06227cf67f098a1a820625d
Vlad Cuciureanu <vlad.cuciureanu@pm.me>
2023-12-19 16:56:28 +0200

    fix: GatherDataEvent import

--- src/main/java/li/cil/oc2/data/DataGenerators.java
+++ src/main/java/li/cil/oc2/data/DataGenerators.java
@@ -5,9 +5,9 @@ package li.cil.oc2.data;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.tags.BlockTagsProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.forge.event.lifecycle.GatherDataEvent;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public final class DataGenerators {
